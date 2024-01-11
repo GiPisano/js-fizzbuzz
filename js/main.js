110fd4d27de22fa8e5d0844d6ce1e642ea78b5e7
GiPisano <pisanogiacomo9@gmail.com>
2024-01-11 16:46:30 +0100

    add correction

--- js/main.js
+++ js/main.js
@@ -8,19 +8,16 @@ for(i = 1; i <= 100; i++){
 
    if(i % 15 == 0){
     boxElement.innerText = 'FizzBuzz';
-    boxElement.style.backgroundColor ="rgb(234,44,92)"
+    boxElement.style.backgroundColor = "rgb(234,44,92)";
     
    } else if(i % 5 == 0){
     boxElement.innerText = 'Buzz';
-    boxElement.style.backgroundColor ="rgb(254,201,84)"
+    boxElement.style.backgroundColor ="rgb(254,201,84)";
    } else if(i % 3 == 0){
     boxElement.innerText = 'Fizz'
-    boxElement.style.backgroundColor ="rgb(30,209,143)"
+    boxElement.style.backgroundColor ="rgb(30,209,143)";
    } else{
     boxElement.innerText = i;
-   }
-
-   
-    
+   } 
 }
 
